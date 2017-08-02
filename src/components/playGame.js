73f4ef959e9f3d5f98389785729a9108334a8637
awbridgers <adamwbridgers@gmail.com>
2017-08-01 21:59:22 -0400

    added all the cards needed for the initial testing of the game

--- src/components/playGame.js
+++ src/components/playGame.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import board from "../images/pazzakBoard.png";
+import board from "../images/pazzakBoard2.png";
 
 
-const boardStyle = {
+let bgDiv = {width: "100%", height: "100%", backgroundColor: "black", textAlign: "center", position: "fixed"};
+let playingBoard = {height: "628px", width:"828px", margin: "auto"}
 
 
 
-}
 
 export default class PlayPazzak extends Component {
   render(){
     return(
-      <div style = {{width: "100%", height: "100%", backgroundColor: "black", textAlign: "center"}}>
-        <div style = {{boardStyle}}><img src = {board}></img></div>
+      <div style = {bgDiv}>
+        <div style = {playingBoard}>
+          <img src = {board}></img>
+        </div>
       </div>
     )
   }
